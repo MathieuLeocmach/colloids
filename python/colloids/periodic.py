f9f2bc3492ba972fe53e05ced616325b322263bb
Mathieu Leocmach <mathieu.leocmach@gmail.com>
2010-09-24 06:51:07 +0000

    isf and msd for periodic boundary conditions

---
+++ python/colloids/periodic.py
@@ -0,0 +1,116 @@
+import numpy as np
+from colloids import vtk
+import os, subprocess, shlex
+
+def get_displ(start, stop, L=203):
+    """Compute displacement with periodic bounday conditions, assuming no particle moved further than L/2"""
+    return np.mod(stop - start + 1.5 * L, L) - 0.5 * L
+
+def grv2vtk(pattern, T, radii=None):
+    positions = np.asarray(
+            [np.loadtxt(pattern%t + '.grv') for t in range(T)]
+            )
+    vel = np.gradient(positions)[0]
+    for t in range(T):
+            v= vtk.Polydata()
+            v.points = positions[t]
+            v.bonds = np.loadtxt(pattern%t + '.bonds', dtype=int)
+            cloud = np.loadtxt(pattern%t+'.cloud')
+            cloudcg = np.loadtxt(pattern%t+'.grv_space.cloud')
+            border = np.zeros(v.bonds.shape[0], bool)
+            border[np.where(
+                ((v.points[v.bonds[:,1]] - v.points[v.bonds[:,0]])**2).sum(axis=1)>50*50)[0]
+                   ]=True
+            v.scalars = [
+                    ('Q6', cloudcg[:,1]),
+                    ('W4', cloudcg[:,4]),
+                    ('w6', cloud[:,5]),
+                    ]
+            if radii != None:
+                v.scalars.append(('radius', radii))
+            v.vectors = [('vel', vel[t])]
+            v.bondsScalars = [('border', border)]
+            v.save(pattern%t+'.vtk')
+
+def remove_drift(a, L=203):
+    mean_displ = np.cumsum(
+        [get_displ(a[i], a[i+1], L).mean(axis=0) for i in range(len(a)-1)],
+        axis=0)
+    for i, d in enumerate(mean_displ):
+		a[i+1] -= d
+    a = np.mod(a + 1.5 * L, L) - 0.5 * L
+
+def msd(A, av=10, L=203):
+    """
+    Mean square displacement in a cubic box of size L
+    If av is 0 (Default), the calculation will act greedily,
+    averaging over all the avilabe intervals of a given length.
+        Example : start=1 stop=4 av=0
+            MSD[0] = 1
+            MSD[1] = ( msd([1,2]) + msd([2,3]) + msd([3,4]))/3
+            MSD[2] = ( msd([1,3]) + msd([2,4]) )/2
+            MSD[3] = msd([1,4])
+    If av>0, the average will be done over av time intervals starting
+    from start, start+1,...,start+av-1
+        Example : start=1 stop=4 av=2
+            MSD[0] = 1
+            MSD[1] = ( msd([1,2]) + msd([2,3]) )/2
+            MSD[2] = ( msd([1,3]) + msd([2,4]) )/2
+            MSD[3] = ( msd([1,4]) + msd([2,5]) )/2
+"""
+    msd = np.zeros(len(A)-av+1)
+    if av==0:
+        for t0, a in enumerate(A):
+            for dt, b in enumerate(A[t0+1:]):
+                #average is done over all trajectories and the 3 dimensions
+                msd[dt+1] += (get_displ(a,b, L)**2).sum()
+        msd /= A.shape[1] * A.shape[2]
+        for dt in range(len(A)):
+            msd[dt] /= len(A)-dt
+        return msd
+    else:
+        for t0, a in enumerate(A[:av]):
+            for dt, b in enumerate(A[t0+1:len(A)-av+t0+1]):
+                msd[dt+1] += (get_displ(a,b, L)**2).sum()
+        msd /= av * A.shape[1] * A.shape[2]
+        return msd
+
+def self_isf(positions, radius, av, L=203):
+    """
+    Self intermediate scattering function
+    If av is 0 (Default), the calculation will act greedily,
+    averaging over all the avilabe intervals of a given length.
+        Example : start=1 stop=4 av=0
+            ISF[0] = 1
+            ISF[1] = ( isf([1,2]) + isf([2,3]) + isf([3,4]))/3
+            ISF[2] = ( isf([1,3]) + isf([2,4]) )/2
+            ISF[3] = isf([1,4])
+If av>0, the average will be done over av time intervals starting
+from start, start+1,...,start+av-1
+        Example : start=1 stop=4 av=2
+            ISF[0] = 1
+            ISF[1] = ( isf([1,2]) + isf([2,3]) )/2
+            ISF[2] = ( isf([1,3]) + isf([2,4]) )/2
+            ISF[3] = ( isf([1,4]) + isf([2,5]) )/2
+"""
+    A = np.exp(
+        positions * (1j * np.pi / radius)
+        )
+    isf = np.zeros(len(A)-av+1)
+    if av==0:
+        for t0, a in enumerate(A):
+            for dt, b in enumerate(A[t0+1:]):
+                #average is done over all trajectories and the 3 dimensions
+                isf[dt+1] += np.real(a.conj()*b).sum()
+        isf /= A.shape[1] * A.shape[2]
+        isf[0]=1
+        for dt in range(len(A)):
+            isf[dt] /= len(A)-dt
+        return isf
+    else:
+        for t0, a in enumerate(A[:av]):
+            for dt, b in enumerate(A[t0+1:len(A)-av+t0+1]):
+                isf[dt+1] += np.real(a.conj()*b).sum()
+        isf /= av * A.shape[1] * A.shape[2]
+        isf[0]=1
+        return isf
