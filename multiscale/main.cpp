b1b9f1f719a42cfb80df2cf3b9d23a3af0c8631f
Mathieu Leocmach <mathieu.leocmach@gmail.com>
2011-11-11 03:32:06 +0000

    prototype of main program

---
+++ multiscale/main.cpp
@@ -0,0 +1,20 @@
+#include "src/multiscalefinder.hpp"
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/progress.hpp>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+using namespace Colloids;
+using namespace boost::posix_time;
+
+int main(int ac, char* av[]){
+	try {
+
+	}
+	catch(exception& e)
+	{
+		cout << e.what() << "\n";
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
