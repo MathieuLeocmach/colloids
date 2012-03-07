c637e3421d42dc2a8f6f6cef3d290356e529c6c4
Mathieu Leocmach <mathieu.leocmach@gmail.com>
2012-03-07 08:34:54 +0000

    fix output file pattern when path is given

--- python/colloids/count_nuclei.py
+++ python/colloids/count_nuclei.py
@@ -2,7 +2,7 @@ import numpy as np
 from colloids import track
 from scipy.misc import imsave, imshow, imread
 import sys, itertools, re
-from os.path import splitext,isfile
+from os.path import split,join,isfile
 
 if len(sys.argv) >1:
     filename = sys.argv[1]
@@ -17,11 +17,15 @@ else:
 m = re.match('(.*)_([0-9]*)(.*)', filename)
 assert m is not None, '"%s" do not match pattern' % filename
 pattern = m.group(1)+'_%'+('0%dd'%len(m.group(2)))+(''.join(m.groups()[2:]))
+path, name = split(m.group(1))
+outpattern = join(path,'treated_'+name)+'_%'+('0%dd'%len(m.group(2)))+(''.join(m.groups()[2:]))
 
 im = imread(filename)
 cent = np.zeros_like(im)
 finder = track.MultiscaleBlobFinder(im.shape, nbOctaves=100)
+
 f = open(m.group(1)+'.nb', 'w')
+
 for t in itertools.count():
     if not isfile(pattern%t):
         break
@@ -32,6 +36,6 @@ for t in itertools.count():
     cent.fill(0)
     for x, y, r, i in centers:
         cent[y-r:y+r+1,x-r:x+r+1] += 50
-    imsave('treated_'+pattern%t, np.dstack((im, cent, cent)))
+    imsave(outpattern%t, np.dstack((im, cent, cent)))
 f.close()
     
