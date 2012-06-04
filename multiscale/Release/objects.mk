abd606107756e1f3f579b75cc1bfdd2e1feb0e56
Mathieu Leocmach <mathieu.leocmach@gmail.com>
2012-06-04 09:53:46 +0000

    add fftw3f

--- multiscale/Release/objects.mk
+++ multiscale/Release/objects.mk
@@ -4,4 +4,4 @@
 
 USER_OBJS :=
 
-LIBS := -lopencv_core -lboost_iostreams -lopencv_imgproc -ltinyxml -lopencv_highgui -lboost_unit_test_framework -lboost_program_options -fopenmp
+LIBS := -lopencv_core -lboost_iostreams -lopencv_imgproc -ltinyxml -lopencv_highgui -lboost_unit_test_framework -lboost_program_options -lfftw3f -fopenmp
