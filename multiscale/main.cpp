57889c516ac4505f0c267b7635e0e9ac9ccb3d3b
Mathieu Leocmach <mathieu.leocmach@gmail.com>
2012-06-26 00:49:55 +0000

    deconvolution parameters

--- multiscale/main.cpp
+++ multiscale/main.cpp
@@ -1,5 +1,6 @@
 #include "src/multiscalefinder.hpp"
 #include "src/lifFile.hpp"
+#include "src/deconvolution.hpp"
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/progress.hpp>
 #include <boost/program_options.hpp>
@@ -17,6 +18,8 @@ int main(int ac, char* av[]){
 		// allowed only on command line
 		po::options_description cmdline_options("Generic options");
 		cmdline_options.add_options()
+			("help", "Show this help and exit")
+			("verbose,v", "Output debugging information")
 			("input,i", po::value<std::string>(&input), "Leica file to read as input")
 			("output,o", po::value<std::string>(&output)->default_value("./output"), "file to output to")
 			("series,s", po::value<int>(&ser), "Dataset number")
@@ -29,8 +32,11 @@ int main(int ac, char* av[]){
 			("incore",
 					"Do not use memory mapped file to store the data. If you have enough RAM this will speed up the calculation. If you don't it will trigger swapping and global slow down or even crash.\n"
 					"If the Octave0 option in ON, the 0th octave still uses memory mapped file.")
-			("verbose,v", "Output debugging information")
-			("help", "Show this help and exit")
+			("deconvolution", po::value<int>()->default_value(0), "Whether to use or not deconvolution and where to get the deconvolution kernel:\n"
+					"(0) Do not use deconvolution."
+					"(1) Compute the kernel. This supposes the sample is physically isotropic but the image is not. If --deconvolution-kernel is given, the result is written to this file.\n"
+					"(2) Load from file, given by --deconvolution-kernel parameter.")
+			("deconvolution-kernel", po::value<std::string>(), "File to read from or write to the deconvolution kernel.")
 			;
 		//Input file as positional option
 		po::positional_options_description p_o;
@@ -78,10 +84,91 @@ int main(int ac, char* av[]){
 			//finder.set_ZXratio(serie.getZXratio()); //DISABLED Particles get lost
 			if(!vm.count("Octave0"))
 				finder.disable_Octave0();
-			//do not preblur in Z to compensate for the point spread function
-			finder.set_halfZpreblur(true);
+
 			//re-open the LIF as a memory mapped file
 			boost::iostreams::mapped_file_source file(vm["input"].as<std::string>());
+
+			//deconvolution kernel
+			std::vector<float> deconv_kernel(dims[2], 1.0f);
+			switch(vm["deconvolution"].as<int>())
+			{
+			case 0:
+				//do not preblur in Z to compensate for the point spread function
+				finder.set_halfZpreblur(true);
+				break;
+
+			case 1:
+			{
+				//check we will be able to output the result
+				if(!vm.count("deconvolution-kernel"))
+				{
+					std::cerr<<cmdline_options << std::endl;
+					std::cerr<<"deconvolution-kernel file needed" << std::endl;
+					return EXIT_FAILURE;
+				}
+				std::ofstream out(vm["deconvolution-kernel"].as<std::string>().c_str());
+				if(!out.good())
+				{
+					std::cerr<<"Cannot open "<< vm["deconvolution-kernel"].as<std::string>() << std::endl;
+					return EXIT_FAILURE;
+				}
+				//load the first image of the series in memory as float
+				cv::Mat_<float> image;
+				cv::Mat(
+						3, dimsint, CV_8UC1, (unsigned char*)(file.data() + serie.getOffset(0))
+						).convertTo(image, image.type());
+				if(!!vm.count("verbose"))
+				{
+					std::cout<<"Image loaded";
+					std::flush(std::cout);
+				}
+				//preblur in all 3 dimensions to eliminate noise
+				inplace_blur3D(image, 1.6, 1.0);
+				if(!!vm.count("verbose"))
+					std::cout<<" and preblurred."<<std::endl;
+				//kernel that would make the Z axis as the X axis
+				deconv_kernel = get_deconv_kernel(image, 2, 0);
+				if(!!vm.count("verbose"))
+					std::cout<<"Deconvolution kernel computed."<<std::endl;
+				//save the kernel to file
+				std::copy(
+						deconv_kernel.begin(), deconv_kernel.end(),
+						std::ostream_iterator<float>(out, "\t"));
+				//load the kernel into the finder
+				finder.load_deconv_kernel(deconv_kernel);
+				finder.set_deconv();
+				break;
+			}
+			case 2:
+			{
+				//load the kernel from file
+				if(!!vm.count("verbose"))
+					std::cout<<"Load the deconvolution kernel from "<< vm["deconvolution-kernel"].as<std::string>() << std::endl;
+				if(!vm.count("deconvolution_kernel"))
+				{
+					std::cerr<<cmdline_options << std::endl;
+					std::cerr<<"deconvolution-kernel file needed" << std::endl;
+					return EXIT_FAILURE;
+				}
+				std::ifstream in(vm["deconvolution-kernel"].as<std::string>().c_str());
+				if(!in.good())
+				{
+					std::cerr<<"Cannot open "<< vm["deconvolution-kernel"].as<std::string>() << std::endl;
+					return EXIT_FAILURE;
+				}
+				deconv_kernel.clear();
+				std::copy(
+						std::istream_iterator<float>(in), std::istream_iterator<float>(),
+						std::back_inserter(deconv_kernel));
+				//load the kernel into the finder
+				finder.load_deconv_kernel(deconv_kernel);
+				finder.set_deconv();
+				break;
+			}
+			}
+
+
+
 			//container for tracked particles
 			std::vector<Center3D> centers;
 			ptime past_total = microsec_clock::local_time();
