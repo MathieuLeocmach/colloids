86926224f99fd022ed59666f4a05184223371398
Mathieu Leocmach <mathieu.leocmach@gmail.com>
2012-08-01 11:09:41 +0000

    Script to compute the spectrum of an image

--- python/colloids/spectrum.py
+++ python/colloids/spectrum.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.misc import imshow, imread
 import sys, itertools, re
 from os.path import split,join,isfile
+import numexpr
 
 if len(sys.argv) >1:
     filename = sys.argv[1]
@@ -20,19 +21,27 @@ path, name = split(m.group(1))
 outpattern = join(path,'spectrum_'+name)+'_%'+('0%dd'%len(m.group(2)))+'.dat'
 
 im = imread(filename)
-dists = np.zeros(im.shape)
-for x, dx in enumerate((np.arange(im.shape[0])-im.shape[0]/2)**2):
-	for y, dy in enumerate((np.arange(im.shape[1])-im.shape[1]/2)**2):
-		dists[x,y] = dx+dy
-dists = np.sqrt(dists)
+shape = im.shape
+wavenumbers = map(np.fft.fftfreq, shape)
+dists = numexpr.evaluate('sqrt(qx**2+qy**2)', {
+    'qx':wavenumbers[0][:,None],
+    'qy':wavenumbers[1][None,:shape[-1]/2+1]
+    })
+nbq, qs = np.histogram(dists.ravel(), wavenumbers[0][:len(wavenumbers[0])/2])
+ws = map(np.hamming, shape)
 
 for t in itertools.count():
     if not isfile(pattern%t):
         break
     im = imread(pattern%t)
-    sp = np.fft.fftshift(np.fft.fft2(im))
-    Sq = np.histogram(dists.ravel(), bins=range(sp.shape[0]/2), weights=np.abs(sp).ravel())[0]
-    nb = np.histogram(dists.ravel(), bins=range(sp.shape[0]/2))[0]
-    Sq[nb>0] /= nb[nb>0]
+    im = im - im.mean()
+    for d, w in enumerate(ws):
+        im *= w[tuple([None]*d + [slice(None)] + [None]*(im.ndim-d-1))]
+    spectrum = numexpr.evaluate(
+        'abs(f).real**2', 
+        {'f': np.fft.rfftn(im)}
+        )
+    Sq = np.histogram(dists.ravel(), qs, weights=spectrum.ravel())[0]
+    Sq[nbq>0] /= nbq[nbq>0]
     np.savetxt(outpattern%t, Sq)
 
