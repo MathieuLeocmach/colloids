c4f21f0e52f952567d1a7e8bd59d67d652157cdf
Mathieu Leocmach <mathieu.leocmach@gmail.com>
2013-01-10 08:15:09 +0000

    nth_period allow direct entry of the blurring radius blurring radius sniffing -1 divide by the background of the first image to get uniform and normalized intensitites. Link present trajectories to trajectories terminated two steps ago. Add intermediate positions. Export the intensities of both full- and half-spanning trajectories. Also export their respective initial positions and successive oscillation periods.

--- python/colloids/track_droplet.py
+++ python/colloids/track_droplet.py
@@ -13,6 +13,12 @@ def readTIFF16(path):
         np.uint8
         ).reshape(tuple(list(im.size)+[2]))
     return (np.array(out[:,:,0], np.uint16) << 8) + out[:,:,1]
+    
+def nth_period(a, n=2, guess=150, width=50):
+    period = guess
+    for i  in range(n):
+        period = (np.argmin(a[period*(i+1)-width:period*(i+1)+width])+period*(i+1)-width)/float(i+1)
+    return period
 
 if __name__ == "__main__":
     #read input file name
@@ -48,15 +54,25 @@ if __name__ == "__main__":
     if im.ndim ==0:
         im = readTIFF16(filename)
     cent = np.zeros_like(im)
-    #estimate the best blurring radius
-    finder = track.MultiscaleBlobFinder(im.shape, nbOctaves=100)
-    centers = finder(im, Octave0=False, removeOverlap=False)
-    sigma = 1.6*2**((1+np.argmax([oc.binary[1:-1].sum(-1).sum(-1) for oc in finder.octaves[1:]]))/3.)
+    if len(sys.argv) >2:
+        sigma = float(sys.argv[2])
+    else:
+        #estimate the best blurring radius
+        finder = track.MultiscaleBlobFinder(im.shape, nbOctaves=100)
+        centers = finder(im, k=1.6, Octave0=False, removeOverlap=False)
+        sigma = 1.6*2**(np.argmax([
+            oc.binary[1:-1].sum(-1).sum(-1) 
+            for oc in finder.octaves[1:]
+            ])/3.0)
     #create a monoscale Crocker & Grier finder
     finder = track.CrockerGrierFinder(im.shape)
+    #background
+    background = track.gaussian_filter(np.array(im, float), 10*sigma)
+    background[background==0] = 1.0
     #create drift file
     fdrift = open(splitext(pattern%0)[0]+'.drift', 'w')
     window = np.hanning(im.shape[0])[:,None]*np.hanning(im.shape[1])
+    drifts = []
 
     for t in itertools.count():
         if not isfile(pattern%t):
@@ -64,8 +80,8 @@ if __name__ == "__main__":
         im = imread(pattern%t)
         if im.ndim ==0:
             im = readTIFF16(pattern%t)
-        #localisation
-        centers = finder(im, sigma)
+        #localisation with background removal
+        centers = finder(im/background, sigma, uniform_size=0)
         #output result image
         cent.fill(0)
         for (ym,xm), (yM, xM) in zip(np.maximum(0, centers[:,:2]-sigma), np.minimum(centers[:,:2]+sigma+1, im.shape[::-1])):
@@ -81,14 +97,57 @@ if __name__ == "__main__":
         else:
             pos0 = pos1
             pos1 = centers
-            #look for overall shift between successive pictures
+            #look for overall drift between successive pictures
             sp0 = sp1
             sp1 = np.fft.fftn(im*window)
             R = sp0 * np.conjugate(sp1)
             shift = phaseCorrelation.getDispl(np.abs(np.fft.ifftn(R/np.abs(R))))
             fdrift.write('%d %d\n'%(shift[1], shift[0]))
+            drifts.append(shift)
             pairs, distances = particles.get_links(pos0[:,:2], np.ones(len(pos0)), pos1[:,:2]+shift[::-1], np.ones(len(pos1)), maxdist=6.*sigma + np.sqrt(np.dot(shift,shift)))
             linker.addFrame(len(pos1), pairs, distances)
+            if t>1:
+                #try to link trajectories terminated two time steps ago with newly created trajectories (no match with one step ago)
+                
+                starting_pos = np.where(linker.pos2tr[-1]>linker.nbtrajs[-2])[0]
+                pairs, distances = particles.get_links(
+                    terminated[:,1:3], 
+                    np.ones(len(terminated)), 
+                    pos1[starting_pos,:2]+shift[::-1], 
+                    np.ones(len(starting_pos)), 
+                    maxdist=6.*sigma + np.sqrt(np.dot(shift,shift))
+                    )
+                if len(pairs)>0:
+                    #translate pairs into indices the linker understands
+                    pairs[:,0] = terminated_tr[pairs[:,0]]
+                    pairs[:,1] = starting_pos[pairs[:,1]]
+                    #update the linker
+                    has_grown = linker.update(pairs, distances)
+                    #add intermediate positions at the end of the previous time step
+                    newly = pos1[has_grown[linker.pos2tr[-1]]]
+                    newly[:,:2] += shift[::-1]
+                    intermediate = 0.5 * (terminated[has_grown[terminated_tr]] + newly)
+                    np.savetxt(
+                        open(splitext(pattern%(t-1))[0]+'.csv', 'a'),
+                        np.column_stack((np.where(has_grown)[0], intermediate)),
+                        fmt='%g')
+                    
+                
+            #remember the trajectories that end at previous time step (not continuing at present time step)
+            terminated_pos_tr = [
+                (tr[-1], i)
+                for i, start, tr in zip(range(linker.nbtrajs[-2]), linker.trajstart, linker.tr2pos)
+                if start+len(tr) == t]
+            if len(terminated_pos_tr)==0:
+                terminated_pos = np.zeros(0,int) 
+                terminated_tr = np.zeros(0,int)
+                terminated = np.zeros([0,3])
+            else:
+                terminated_pos, terminated_tr = np.transpose(terminated_pos_tr)
+                terminated = pos0[terminated_pos]
+                terminated[:,:2] -= shift[::-1]
+        #remember inner image of the traker to measure intensities afterward
+        blurred0 = np.copy(finder.blurred)
         #output
         np.savetxt(
             splitext(pattern%t)[0]+'.csv', 
@@ -96,11 +155,37 @@ if __name__ == "__main__":
             fmt='%g'
             )
     fdrift.close()
+    #Focus on the trajectories spanning the whole time and half of it
     isspanning = np.array(map(len, linker.tr2pos)) == len(linker.pos2tr)
+    halfspanning = np.intersect1d(linker.pos2tr[0], linker.pos2tr[len(linker.pos2tr)/2])
+    ishalfspanning = np.zeros(len(linker.tr2pos), bool)
+    ishalfspanning[halfspanning] = True
     intensities = np.zeros((len(linker.pos2tr), isspanning.sum()))
+    halfinten = np.zeros((len(linker.pos2tr)/2, len(halfspanning)))
     for t, pos2tr in enumerate(linker.pos2tr):
         name = splitext(pattern%t)[0]+'.csv'
-        trs = np.loadtxt(name, usecols=[0], dtype=int)
-        intensities[t] = np.loadtxt(name, usecols=[3])[isspanning[pos2tr]][np.argsort(pos2tr[isspanning[pos2tr]])]
-    np.savetxt(splitext(pattern%t)[0]+'.intensities', intensities, fmt='%g')
+        alli = np.loadtxt(name, usecols=[3])
+        intensities[t] = alli[isspanning[pos2tr]][np.argsort(pos2tr[isspanning[pos2tr]])]
+        if t<len(halfinten):
+            halfinten[t] = alli[ishalfspanning[pos2tr]][np.argsort(pos2tr[ishalfspanning[pos2tr]])]
+            #halfinten[t] = alli[np.argsort(pos2tr[ishalfspanning[pos2tr]])]
+    np.savetxt(join(path,'intensities.csv'), intensities, fmt='%g')
+    np.savetxt(join(path,'half_intensities.csv'), halfinten, fmt='%g')
+    #compute periods and export them with the positions in the first frame
+    first = np.loadtxt(splitext(pattern%0)[0]+'.csv', usecols=[1,2])
+    periods = []
+    for n in itertools.count():
+        try:
+            periods.append([nth_period(i,1+n) for i in intensities.T])
+        except ValueError:
+            break
+    np.savetxt(join(path,'pos_periods.csv'), np.column_stack([first[isspanning[linker.pos2tr[0]]]]+periods), fmt='%g')
+    periods = []
+    for n in itertools.count():
+        try:
+            periods.append([nth_period(i,1+n) for i in halfinten.T])
+        except ValueError:
+            break
+    np.savetxt(join(path,'pos_halfperiods.csv'), np.column_stack([first[ishalfspanning[linker.pos2tr[0]]]]+periods), fmt='%g')
+    
     
