f87b5867f33c27583a4f9a868e96b1fc84cc828c
Mathieu Leocmach <mathieu.leocmach@gmail.com>
2015-05-13 23:56:00 +0000

    Possible to use Kolafa correction to Carnahan&Starling equation of states.

--- python/colloids/phase.py
+++ python/colloids/phase.py
@@ -9,9 +9,9 @@ from scipy import interpolate
 def logOnePlusX(x):
     """Calculate log(1 + x), preventing loss of precision for small values of x."""
     #assert x > -1.0, "Invalid input argument (%f); must be greater than -1.0"%x
-    if np.fabs(x) > 0.375:
+    #if np.fabs(x) > 0.375:
         # x is sufficiently large that the obvious evaluation is OK
-        return np.log(1.0 + x);
+        #return np.log(1.0 + x);
     # For smaller arguments we use a rational approximation
     # to the function log(1+x) to avoid the loss of precision
     # that would occur if we simply added 1 to x then took the log.
@@ -24,7 +24,7 @@ def logOnePlusX(x):
     t = x/(x + 2.0)
     t2 = t*t
     w = (((p3*t2 + p2)*t2 + p1)*t2 + 1.0)/(((q3*t2 + q2)*t2 + q1)*t2 + 1.0);
-    return 2.0*t*w;
+    return np.where(np.fabs(x) > 0.375, np.log(1.0 + x), 2.0*t*w);
 
 eta_cp = np.pi/6*np.sqrt(2)
 f_cp = eta_cp/(1 - eta_cp)
@@ -40,19 +40,55 @@ beta = lambda f,q: np.exp(-Q(f,q))
 beta1 = lambda f,q: -beta(f,q)*Q1(f,q)
 beta2 = lambda f,q: -beta(f,q)*Q2(f,q) - beta1(f,q)*Q1(f,q)
 beta3 = lambda f,q: -beta(f,q)*Q3(f,q) - 2*beta1(f,q)*Q2(f,q)- beta2(f,q)*Q1(f,q)
-pv_0 = lambda f: f/(1+f) + 4*f**2 + 2* f**3
+
+def pv_0(f, Kolafa=False):
+    """HS fluid equation of state, or phi*Z(phi)"""
+    if not Kolafa:
+        #Carnahan-Starling
+        return f/(1+f) + 4*f**2 + 2* f**3
+    #Kolafa
+    return (3 + 15*f + 30*f**2 + 25*f**3 + 5*f**4)*f/(1+f)**2/3. 
+    
 pvS_0 = lambda f: 3/(1.0/f - 1/f_cp)
-pv_0_1 = lambda f: (1+f)**(-2) + 8*f + 6*f**2
-pv_0_2 = lambda f: -2*(1+f)**(-3) + 8 + 12*f
-mu_0 =  lambda f: np.log(f) - logOnePlusX(f) + 8*f + 7*f**2 + 2*f**3
+
+def pv_0_1(f, Kolafa=False):
+    """First derivative of HS fluid equation of state""" 
+    if not Kolafa:
+        #Carnahan-Starling
+        return (1+f)**(-2) + 8*f + 6*f**2
+    #Kolafa
+    return 5*f**2 + 10*f + 4*(1+f)**(-2) - 4/3. * (1+f)**(-3) - 5/3.
+
+def pv_0_2(f, Kolafa=False):
+    """Second derivative of HS fluid equation of state""" 
+    if not Kolafa:
+        #Carnahan-Starling
+        return -2*(1+f)**(-3) + 8 + 12*f
+    #Kolafa
+    return 10*(1+f) -8*(1+f)**(-3) +4*(1+f)**(-4)
+
+def mu_0(f, Kolafa=False):
+    """HS fluid chemical potential"""
+    if not Kolafa:
+        #from Carnahan-Starling
+        return np.log(f) - logOnePlusX(f) + 8*f + 7*f**2 + 2*f**3
+    #from Kolafa
+    return np.log(f) + (-8*logOnePlusX(f) + 5 - 4/(f+1) + 25*f + 45*f**2/2. + 5*f**3) / 3.
 def muS_0(f):
+    """HS crystal chemical potential"""
     u = 1/f - 1/f_cp
     return 2.1306 + 3.0*(1+f)/u/f - 3*np.log(u)
 g = lambda f,q: (beta(f,q) - (1+f)*beta1(f,q))/(1+q)**3
 h = lambda f,q: beta(f,q) - f*beta1(f,q)
-pv = lambda f, piv, q: pv_0(f) + piv * h(f,q)
+
+def pv(f, piv, q, Kolafa=False):
+    return pv_0(f, Kolafa) + piv * h(f,q)
+
 pvS = lambda f, piv, q: pvS_0(f) + piv * h(f,q)
-mu = lambda f, piv, q: mu_0(f) + piv * (1+q)**3 * g(f, q)
+
+def mu(f, piv, q, Kolafa=False):
+    return mu_0(f, Kolafa) + piv * (1+q)**3 * g(f, q)
+    
 muS = lambda f, piv, q: muS_0(f) + piv * (1+q)**3 * g(f, q)
 alpha = lambda f, q: beta(f,q)/(1+f)
 qR2q = lambda qR: 0.9*qR**0.9
@@ -61,58 +97,71 @@ y2piv = lambda y, qR: y/qR**3
 f2vf = lambda f: f/(1+f)
 vf2f = lambda vf: vf/(1-vf)
 
-def mu_of_log(F, piv, q):
+def mu_of_log(F, piv, q, Kolafa=False):
+    """Redefine the fluid chemical potential with F=log(f) as variable. Prevents numerical errors when doing exp(log(f))""" 
     f = np.exp(F)
-    return F - logOnePlusX(f) + 8*f + 7*f**2 + 2*f**3  + piv * (1+q)**3 * g(f, q)
+    if not Kolafa:
+        #Carnahan-Starling
+        return F - logOnePlusX(f) + 8*f + 7*f**2 + 2*f**3  + piv * (1+q)**3 * g(f, q)
+    #Kolafa expression
+    return F + (-8*logOnePlusX(f) + 5 - 4/(f+1) + 25*f + 45*f**2/2. + 5*f**3) / 3.  + piv * (1+q)**3 * g(f, q)
+    
 def muS_of_U (U, piv, q):
+    """Redefine the crystal chemical potential with U=log(u) as variable. Prevents numerical errors when doing exp(log(u))"""
     u = np.exp(U)
     f = 1.0/(u+1/f_cp)
     return 2.1306 + 3.0*(1+1/eta_cp/u) - 3*U + piv * (1+q)**3 * g(f, q)
-mu_of_U = lambda U, piv, q: mu_of_log(-np.log(np.exp(U) + 1/f_cp), piv, q)
 
-pv_of_log = lambda F, piv, q: pv(np.exp(F), piv, q)
+def mu_of_U(U, piv, q, Kolafa=False):
+    return mu_of_log(-np.log(np.exp(U) + 1/f_cp), piv, q, Kolafa)
+
+def pv_of_log(F, piv, q, Kolafa=False):
+    return pv(np.exp(F), piv, q, Kolafa)
+    
 pvS_of_log = lambda F, piv, q: pvS(np.exp(F), piv, q)
-pv_of_U = lambda U, piv, q: pv(1/(np.exp(U) + 1/f_cp), piv, q)
+def pv_of_U(U, piv, q, Kolafa=False):
+    return pv(1/(np.exp(U) + 1/f_cp), piv, q, Kolafa)
+    
 def pvS_of_U(U, piv, q):
     u = np.exp(U)
     f = 1.0/(u+1/f_cp)
     return 3/u + piv * h(f,q)
 
-def critical_point(q):
+def critical_point(q, Kolafa=False):
     """Critical point coordinates in the (f,PIv) plane function of the effective size ratio q=delta/a"""
-    fc = fsolve(lambda f: 1/f + beta3(f,q)/beta2(f,q) - pv_0_2(f)/pv_0_1(f), 0.5)[0]
-    PIvc = pv_0_1(fc)/fc/beta2(fc, q)
+    fc = fsolve(lambda f: 1/f + beta3(f,q)/beta2(f,q) - pv_0_2(f, Kolafa)/pv_0_1(f, Kolafa), 0.5)[0]
+    PIvc = pv_0_1(fc, Kolafa)/fc/beta2(fc, q)
     return (fc, PIvc)
     
-def binodalGL(piv, q, guess=None):
+def binodalGL(piv, q, guess=None, Kolafa=False):
     """return (log(f_gas), log(f_liquid)) on the binodal line at a given insersion work piv"""
     if guess is None:
-        fc = critical_point(q)[0]
-        fspG = fminbound(lambda f: -pv(f, piv,q), 0, fc)
-        fspL = fminbound(pv, fc, 2*fc, args=(piv, q))
+        fc = critical_point(q, Kolafa)[0]
+        fspG = fminbound(lambda f: -pv(f, piv,q, Kolafa), 0, fc)
+        fspL = fminbound(pv, fc, 2*fc, args=(piv, q, Kolafa))
         guess = np.log([0.5*fspG, fspL+fspG])
     return fsolve(lambda Fs: [
-        pv_of_log(Fs[0], piv, q) - pv_of_log(Fs[1], piv, q), 
-        mu_of_log(Fs[0], piv, q) - mu_of_log(Fs[1], piv, q)
+        pv_of_log(Fs[0], piv, q, Kolafa) - pv_of_log(Fs[1], piv, q, Kolafa), 
+        mu_of_log(Fs[0], piv, q, Kolafa) - mu_of_log(Fs[1], piv, q, Kolafa)
         ], guess)
 
-def spinodalGL(q, bins=None):
-    fc, pivc = critical_point(q)
+def spinodalGL(q, bins=None, Kolafa=False):
+    fc, pivc = critical_point(q, Kolafa)
     if bins is None:
         bins = 1/np.linspace(1/pivc, 1/(8*pivc))
     return np.column_stack((bins, np.vstack([(
-        fminbound(lambda f: -pv(f, piv, q), 0, fc),
-        fminbound(lambda f: pv(f, piv, q), fc, 12)
+        fminbound(lambda f: -pv(f, piv, q, Kolafa), 0, fc),
+        fminbound(lambda f: pv(f, piv, q, Kolafa), fc, 12)
         ) for piv in bins])))
     
         
-def all_GL(q, maxpiv=None):
-    fc, pivc = critical_point(q)
+def all_GL(q, maxpiv=None, Kolafa=False):
+    fc, pivc = critical_point(q, Kolafa)
     Fc = np.log(fc)
     #start sensibly above the critical point
     startp = pivc*1.1
-    fm = fminbound(mu, fc, 2*fc, args=(startp, q))
-    fM = fminbound(lambda f: -pv(f, startp, q), 0, fc)
+    fm = fminbound(mu, fc, 2*fc, args=(startp, q, Kolafa))
+    fM = fminbound(lambda f: -pv(f, startp, q, Kolafa), 0, fc)
     initial_guess = np.log([0.5*fM, fm+fM])
     #construct the top of the GL binodal
     if maxpiv is None:
@@ -120,12 +169,12 @@ def all_GL(q, maxpiv=None):
     topp = 1./np.linspace(1./startp, 1./maxpiv)
     topGL = [initial_guess]
     for piv in topp:
-        topGL.append(binodalGL(piv, q, topGL[-1]))
+        topGL.append(binodalGL(piv, q, topGL[-1], Kolafa))
     #construct the GL binodal between the starting piv and the critical point
     botp = np.linspace(startp, pivc)[:-1]
     botGL = [initial_guess]
     for piv in botp:
-        botGL.append(binodalGL(piv, q, botGL[-1]))
+        botGL.append(binodalGL(piv, q, botGL[-1], Kolafa))
     #join the two results
     binodal = np.vstack((
         [[pivc, fc, fc]],
@@ -133,22 +182,24 @@ def all_GL(q, maxpiv=None):
         np.column_stack((topp, np.exp(topGL[1:])))[1:]
         ))
     #spinodal
-    spinodal = spinodalGL(q, binodal[:,0])
+    spinodal = spinodalGL(q, binodal[:,0], Kolafa)
     #join everything
     return np.column_stack((binodal, spinodal[:,1:]))
     
-def binodalFS(piv, q, guess=[0.970, 1.185]):
+def binodalFS(piv, q, guess=[0.970, 1.185], Kolafa=False):
     """return (f_Fluid, f_solid) on the binodal line at a given insersion work piv"""
+    #adjust solid chemical potential to ensure HS coexistence
+    Delta_muS_0 = muS_of_U(np.log(1/1.185-1/f_cp), 0, q) - mu_of_U(np.log(1/0.970-1/f_cp), 0, q, Kolafa)
     Us0 = np.log(1./np.array(guess) - 1/f_cp)
     result = fsolve(lambda Us: [
-        pv_of_U(Us[0], piv, q) - pvS_of_U(Us[1], piv, q), 
-        mu_of_U(Us[0], piv, q) - muS_of_U(Us[1], piv, q)
+        pv_of_U(Us[0], piv, q, Kolafa) - pvS_of_U(Us[1], piv, q), 
+        mu_of_U(Us[0], piv, q, Kolafa) - muS_of_U(Us[1], piv, q) + Delta_muS_0
         ], Us0)
     return 1./(np.exp(result)+1/f_cp)
     
-def all_FS(q, maxpiv=None, topp=None):
+def all_FS(q, maxpiv=None, topp=None, Kolafa=False):
     if topp is None:
-        fc, pivc = critical_point(q)
+        fc, pivc = critical_point(q, Kolafa)
         if maxpiv is None:
             maxpiv = pivc*2
         #need to integrate from top to bottom or it gets unstable
@@ -158,7 +209,7 @@ def all_FS(q, maxpiv=None, topp=None):
             )[::-1]
     topFS = [[1e-3, f_cp-1e-3]]
     for piv in topp:
-        topFS.append(binodalFS(piv, q, topFS[-1]))
+        topFS.append(binodalFS(piv, q, topFS[-1], Kolafa))
     return np.vstack((
         [0, 0.970, 1.185],
         np.column_stack((topp, topFS[1:]))[::-1]
