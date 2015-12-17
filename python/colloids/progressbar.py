235a7f9e4d4461130cf528740338f3779b58c7c7
Mathieu Leocmach <mathieu.leocmach@gmail.com>
2015-12-17 16:25:04 +0100

    use FloatProgress when possible

--- python/colloids/progressbar.py
+++ python/colloids/progressbar.py
@@ -4,6 +4,13 @@ import sys, time
 try:
     from IPython.core.display import clear_output
     have_ipython = True
+    try:
+        from ipykernel.zmqshell import ZMQInteractiveShell
+        from ipywidgets import FloatProgress
+        from IPython.display import display
+        in_notebook = isinstance(get_ipython(), ZMQInteractiveShell)
+    except ImportError:
+        in_notebook = False
 except ImportError:
     have_ipython = False
 
@@ -21,7 +28,12 @@ class ProgressBar:
         self.width = 40
         self.__update_amount(0)
         if have_ipython:
-            self.animate = self.animate_ipython
+            if(in_notebook):
+                self.animate = self.animate_notebook
+                self.fp = FloatProgress(min=0, max=self.iterations)
+                display(self.fp)
+            else:
+                self.animate = self.animate_ipython
         else:
             self.animate = self.animate_noipython
 
@@ -31,9 +43,12 @@ class ProgressBar:
         except Exception:
             # terminal IPython has no clear_output
             pass
-        print '\r', self,
+        print(self, end='\r')
         sys.stdout.flush()
         self.update_iteration(iter + 1)
+        
+    def animate_notebook(self, i):
+        self.fp.value = i
 
     def update_iteration(self, elapsed_iter):
         self.__update_amount((elapsed_iter / float(self.iterations)) * 100.0)
@@ -44,7 +59,7 @@ class ProgressBar:
         all_full = self.width - 2
         num_hashes = int(round((percent_done / 100.0) * all_full))
         self.prog_bar = '[' + self.fill_char * num_hashes + ' ' * (all_full - num_hashes) + ']'
-        pct_place = (len(self.prog_bar) / 2) - len(str(percent_done))
+        pct_place = (len(self.prog_bar) // 2) - len(str(percent_done))
         pct_string = '%d%%' % percent_done
         self.prog_bar = self.prog_bar[0:pct_place] + \
             (pct_string + self.prog_bar[pct_place + len(pct_string):])
