6dcca44766eaf40ecb8d8b27df3166c5ab48400d
Mathieu Leocmach <mathieu.leocmach@gmail.com>
2017-11-03 23:47:52 +0100

    Instead of periodic_qlm, call periodify from bonds2qlm with optional argument 'periods'

--- python/colloids/boo.py
+++ python/colloids/boo.py
@@ -86,61 +86,6 @@ def weave_qlm(pos, ngbs, inside, l=6):
         extra_link_args=['-lgomp'],
         verbose=2, compiler='gcc')
     return qlm
-    
-def periodic_qlm(pos, ngbs, period, l=6, weights=None):
-    assert len(pos) == len(ngbs)
-    if weights is None:
-        weights = np.ones(ngbs.shape, float)/ngbs.shape[1]
-    assert weights.shape == ngbs.shape
-    qlm = np.zeros([len(pos), l+1], np.complex128)
-    code = """
-    #pragma omp parallel for
-    for(int i=0; i<Nngbs[0]; ++i)
-    {
-        double cart[3] = {0, 0, 0};
-        double sph[3] = {0, 0, 0};
-        //int nb = 0;
-        double total_weight = 0.0;
-        for(int j=0; j<Nngbs[1]; ++j)
-        {
-            int q = ngbs(i,j);
-            if(q<0 || q>=Npos[0])
-                continue;
-            //nb++;
-            double weight = weights(i,j);
-            total_weight += weight;
-            for(int d=0; d<3; ++d)
-                cart[d] = periodic_dist(pos(i,d), pos(q, d), period);
-            sph[0] = sqrt(cart[0]*cart[0] + cart[1]*cart[1] + cart[2]*cart[2]);
-            if(abs(cart[2])==sph[0] || sph[0]*sph[0]+1.0 == 1.0)
-            {
-                sph[1] = 0;
-                sph[2] = 0;
-            }
-            else
-            {
-                sph[1] = acos(cart[2]/sph[0]);
-                sph[2] = atan2(cart[1], cart[0]);
-                if(sph[2]<0)
-                    sph[2] += 2.0*M_PI;
-            }
-            for(int m=0; m<Nqlm[1]; ++m)
-                qlm(i,m) += weight * boost::math::spherical_harmonic(Nqlm[1]-1, m, sph[1], sph[2]);
-        }
-        if(total_weight>0)
-            for(int m=0; m<Nqlm[1]; ++m)
-                qlm(i,m) /= total_weight;
-    }
-    """
-    weave.inline(
-        code,['pos', 'ngbs', 'period', 'weights', 'qlm'],
-        type_converters =converters.blitz,
-        headers = ['<boost/math/special_functions/spherical_harmonic.hpp>'],
-        support_code = periodic.dist_code,
-        extra_compile_args =['-O3 -fopenmp'],
-        extra_link_args=['-lgomp'],
-        verbose=2, compiler='gcc')
-    return qlm
 
 def cart2sph(cartesian):
     """Convert Cartesian coordinates [[x,y,z],] to spherical coordinates [[r,phi,theta],]
@@ -177,11 +122,18 @@ def single_pos2qlm(pos, i, ngb_indices, l=6):
     vectors = pos[ngb_indices]-pos[i]
     return vect2Ylm(vectors, l).mean(-1)
     
-def bonds2qlm(pos, bonds, l=6):
+def bonds2qlm(pos, bonds, l=6, periods=None):
     """Returns the qlm for every particle"""
     qlm = np.zeros((len(pos), l+1), np.complex128)
     #spherical harmonic coefficients for each bond
-    Ylm = vect2Ylm(pos[bonds[:,1]] - pos[bonds[:,0]], l).T
+    Ylm = vect2Ylm(
+        periodic.periodify(
+            pos[bonds[:,0]],
+            pos[bonds[:,1]],
+            periods
+        ),
+        l
+    ).T
     #bin bond into each particle belonging to it
     np.add.at(qlm, bonds[:,0], Ylm)
     np.add.at(qlm, bonds[:,1], Ylm)
